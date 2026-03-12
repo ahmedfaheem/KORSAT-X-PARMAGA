8ad91f8375133740d2120bc0eb556b9446ea74bc
Ahmed Faheem <ahmedfaheem499@gmail.com>
2026-03-12 02:54:19 +0200

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+        /*
+        color:{
+        primary: "#ff3333",
+        secondary: {
+            100: "#9dc3ff",
+            200: "#5b9aff"
+        }
+        },
+        fontFamily:{
+            bodyFont:['Poppins'],
+            secitonFont:[''],
+        }
+            */
+    },
+  },
+  plugins: [],
+};
+
+
+
